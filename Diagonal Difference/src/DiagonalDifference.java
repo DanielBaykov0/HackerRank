7d6c66d3f26e149551cac11e1861fdff55017477
Daniel Baykov <danibaikov@gmail.com>
2023-11-07 21:24:55 +0200

    adding Diagonal Difference

---
+++ Diagonal Difference/src/DiagonalDifference.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class DiagonalDifference {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = DiagonalDifference.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int diagonal1 = 0;
+        int diagonal2 = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            diagonal1 += arr.get(i).get(i);
+            diagonal2 += arr.get(i).get(arr.size() - i - 1);
+        }
+
+        return Math.abs(diagonal1 - diagonal2);
+    }
+}
